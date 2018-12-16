30c6c4cd8f93630ea7cbbc1b9596fb5da73e464e
unknown <pp_nancy@hotmail.com>
2018-12-16 15:46:46 -0500

    added files

---
+++ assets/javascript/game.js
@@ -0,0 +1,58 @@
+
+    // Set computer choiced 
+      var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+ 
+      var wins = 0;
+      var losses = 0;
+      var numGuesses = 9;
+      var guessChoices = [];
+
+      // set user choices
+
+ 
+      document.onkeyup = function(event) {
+ 
+        var userGuess = event.key;
+ 
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+ 
+        var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+        
+ 
+       if (options.indexOf(userGuess) > -1) {
+ 
+         if (userGuess === computerGuess) {
+           wins++;
+           numGuesses = 9;
+           guessChoices = [];
+         }
+ 
+         if (userGuess !== computerGuess) {
+           numGuesses --;
+           guessChoices.push(userGuess);
+         }
+ 
+         if (numGuesses === 0) {
+ 
+         numGuesses = 9;
+         losses ++;
+         guessChoices = [];
+
+         }  
+        }
+ 
+       var html = 
+       
+        "<h1> The Psychic Game </h1>"+
+        "<h6>Guess what an alphabet that I am thinking of!</h6>"+
+        "<p> Press one of alphabet keys on your keyboard.   </p>"+
+        "<h3>Wins: " + wins + "</h3>" +
+        "<h3>Losses: " + losses + "</h3>" +
+        "<h3>Guesses Left: " + numGuesses + "</h3>" +
+        "<h3>Your Guesses so far: " + guessChoices.join(", ") + "</h3>";
+ 
+       document.querySelector("html").innerHTML = html;
+ 
+       
+       }
+      
