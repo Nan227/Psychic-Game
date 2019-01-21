ccbde6d2feec47bd15c6df0e5abeb1ec9022ecb2
unknown <pp_nancy@hotmail.com>
2019-01-21 01:54:54 -0500

    Added the Read me file and added a picture of project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,59 @@
 
-    // Set computer choiced 
-      var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
- 
-      var wins = 0;
-      var losses = 0;
-      var numGuesses = 9;
-      var guessChoices = [];
+//Setting variables and zeroing counters 
+var won = 0;
+var lost = 0;
+var attempts = 10;
+var usedArray = [];
+var ranLetter = ranLetter;
+var letters = "qwertyuiopasdfghjklzxcvbnm"
 
-      // set user choices
+//GENERATING THE COMPUTERS SECRET LETTER
 
- 
-      document.onkeyup = function(event) {
- 
-        var userGuess = event.key;
- 
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
- 
-        var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-        
- 
-       if (options.indexOf(userGuess) > -1) {
- 
-         if (userGuess === computerGuess) {
-           wins++;
-           numGuesses = 9;
-           guessChoices = [];
-         }
- 
-         if (userGuess !== computerGuess) {
-           numGuesses --;
-           guessChoices.push(userGuess);
-         }
- 
-         if (numGuesses === 0) {
- 
-         numGuesses = 9;
-         losses ++;
-         guessChoices = [];
+function jsGuess() {
+      ranLetter = letters[Math.floor(Math.random() * letters.length)];
+      console.log(ranLetter);
 
-         }  
-        }
- 
-       var html = 
-       
-        "<h1> The Psychic Game </h1>"+
-        "<h6>Guess what an alphabet that I am thinking of!</h6>"+
-        "<p> Press one of alphabet keys on your keyboard.   </p>"+
-        "<h3>Wins: " + wins + "</h3>" +
-        "<h3>Losses: " + losses + "</h3>" +
-        "<h3>Guesses Left: " + numGuesses + "</h3>" +
-        "<h3>Your Guesses so far: " + guessChoices.join(", ") + "</h3>";
- 
-       document.querySelector("html").innerHTML = html;
- 
-       
-       }
+}
+
+//CAPTURING THE PLAYERS INPUT
+//.onkeyup captures the players input as playerGuess
+document.onkeyup = function (event) {
+      var playerGuess = event.key;
+
+      // HANDELING CORRECT GUESSES
+      //test if players guess equals ranLetter, if true it increments wins by 1, and clears used letters array. Supposed to reset guess # to 10 but starts at 9 instead, 
+      if (playerGuess === ranLetter) {
+            won++;
+            attempts = 10;
+            usedArray = [];
+      }
+
+      //HANDELING INCORRECT GUESSES
+      //tests if players guess Does Not Equal ranLetter and decriments attempts by 1
+      jsGuess();
+      if (playerGuess !== ranLetter && playerGuess!==usedArray) {
+            attempts--;
+      }
+      //when remaining attempts equals zero, lost is incrimented by 1; attempts is reset to 10, and used letters array is cleared
+      if (attempts == 0) {
+            lost++;
+            usedArray = []
+            attempts = 10;
+      }
+      //HANDELING INCORRECT GUESSES - OUTPUT
+      //this 'if' prevents a letter selected a 2nd time from being written to the usedArray again, although it still counts as a guess
+      if (usedArray.indexOf(playerGuess) >= 0) {
+
+      } else {
+            //this pushes the players incorrect guess to the usedArray and writes it to the HTML
+            usedArray.push(playerGuess);
+            document.getElementById('playerGuess').innerHTML = usedArray;
+            console.log(usedArray);
+      }
+      //OUTPUT TO HTML
       
+      document.getElementById('wins').innerHTML = won;
+      document.getElementById('losses').innerHTML = lost;
+      document.getElementById('guessesLeft').innerHTML = attempts;
+
+}
